a96659b7344f1cfc701bc17122f4608aae37151b
kntokgw-pixel-india <96171676+kanta-okagawa@users.noreply.github.com>
2026-07-12 19:58:22 +0900

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        text = ''
+        for i in s:
+            if i.isalnum():
+                text += i.lower()
+        return text == text[::-1]
